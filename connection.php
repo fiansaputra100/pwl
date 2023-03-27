6bb80f1f5b9e52ffc198e18d8939e616d6fc511b
FianRifkySaputra <fiansaputra100@gmail.com>
2023-03-27 10:47:22 +0700

    update proyek 1 27

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php 
+    $hostname = "Localhost";
+    $username = "root";
+    $password = "";
+    $database = "pemesanan_tiket_liburan";
+    
+    $connect = mysqli_connect($hostname, $username, $password, $database);
+
+?>
